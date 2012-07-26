8ad935a2b63a068eaa6c3616c5a46a78cb836a93
camilstaps <camilstaps@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-26 18:16:47 +0000

    1.2
    git-svn-id: https://plugins.svn.wordpress.org/nanowrimo-stats/trunk@577793 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nanostats.php
+++ nanostats.php
@@ -1,9 +1,9 @@
 <?php
 /*
 Plugin Name: NaNoWriMo Stats
-Plugin URI: http://plugins.camilstaps.nl/nanowrimo-stats/
+Plugin URI: http://plugins.camilstaps.nl/plugins/nanowrimo-stats/
 Description: Allows you to show your NaNoWriMo Stats in posts, pages and sidebar widgets.
-Version: 1.1
+Version: 1.2
 Author: Camil Staps
 Author URI: http://camilstaps.nl
 License: GPL2
@@ -24,6 +24,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 */
+
+error_reporting(E_ALL);
 
 /* Runs when plugin is activated */
 register_activation_hook(__FILE__,'nanostats_install');
@@ -68,152 +70,7 @@ if (is_admin()) {
 	}
 	
 	function nanostats_option_page() {
-		register_setting('nanostats_basic','nanostats_username');
-		register_setting('nanostats_basic','nanostats_region');
-		register_setting('nanostats_basic','nanostats_width');
-		register_setting('nanostats_basic','nanostats_height');
-		register_setting('nanostats_basic','nanostats_color_wordcount');
-		register_setting('nanostats_basic','nanostats_color_goal');
-		register_setting('nanostats_basic','nanostats_color_daily');
-		register_setting('nanostats_basic','nanostats_title');
-		?>
-		<div class="wrap">
-			<style type="text/css">
-				pre {
-					display: inline;
-				}
-				ul {
-					list-style-type: disc;
-					padding-left: 20px;
-				}
-			</style>
-			<form method="post" action="">
-			<?php 
-				settings_fields('nanostats_basic'); 
-				if (isset($_POST['nanostats_username'])) update_option('nanostats_username', $_POST['nanostats_username']);
-				if (isset($_POST['nanostats_region'])) update_option('nanostats_region', $_POST['nanostats_region']);
-				if (isset($_POST['nanostats_width'])) update_option('nanostats_width', $_POST['nanostats_width']);
-				if (isset($_POST['nanostats_height'])) update_option('nanostats_height', $_POST['nanostats_height']);
-				if (isset($_POST['nanostats_color_wordcount'])) update_option('nanostats_color_wordcount', $_POST['nanostats_color_wordcount']);
-				if (isset($_POST['nanostats_color_goal'])) update_option('nanostats_color_goal', $_POST['nanostats_color_goal']);
-				if (isset($_POST['nanostats_color_daily'])) update_option('nanostats_color_daily', $_POST['nanostats_color_daily']);
-				if (isset($_POST['nanostats_title'])) update_option('nanostats_title', $_POST['nanostats_title']);
-				if (isset($_POST['nanostats_username'])) {
-					?><div class="updated"><p><strong><?php _e('Options saved.', 'mt_trans_domain' ); ?></strong></p></div><?php
-				}
-			?>
-			<h2>NaNoWriMo Stats Options</h2>
-				Need any help? Please have a look at the <a href="http://plugins.camilstaps.nl/nanowrimo-stats">documentation</a>. There's a <a href="http://plugins.camilstaps.nl/support">Support page</a> too.<br/>
-				<table class="form-table">
-					<tr valign="top">
-						<th scope="row">Default username</th>
-						<td><input type="text" name="nanostats_username" value="<?php echo get_option('nanostats_username'); ?>" /></td>
-						<td>Unless otherwise specified, this username will be used to show the statistics. When in a WordWar widget less than 2 usernames are given, this one will be added.</td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">Default region</th>
-						<td><input type="text" name="nanostats_region" value="<?php echo get_option('nanostats_region'); ?>" /></td>
-						<td>The default region. When in a RegionWar widget less than 2 regions are given, this one will be added.
-							<br/>You can lookup your region by going to <a target="_blank" href="http://nanowrimo.org/en/regions">http://nanowrimo.org/en/regions</a>, click on your region and copy the region from the URL. For example, my region is <a target="_blank" href="http://nanowrimo.org/en/regions/europe-holland-belgium">http://nanowrimo.org/en/regions/europe-holland-belgium</a>, so my region is "europe-holland-belgium".</td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">Default width</th>
-						<td><input type="text" name="nanostats_width" value="<?php echo get_option('nanostats_width'); ?>" /></td>
-						<td>In pixels.</td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">Default height</th>
-						<td><input type="text" name="nanostats_height" value="<?php echo get_option('nanostats_height'); ?>" /></td>
-						<td>In pixels.</td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">Default WC color</th>
-						<td><input type="text" name="nanostats_color_wordcount" value="<?php echo get_option('nanostats_color_wordcount'); ?>" /></td>
-						<td>The default color for the wordcount bars. (default: #8888cc)</td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">Default goal color</th>
-						<td><input type="text" name="nanostats_color_goal" value="<?php echo get_option('nanostats_color_goal'); ?>" /></td>
-						<td>The default color for the goal line. (default: #674732)</td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">Default todays words color</th>
-						<td><input type="text" name="nanostats_color_daily" value="<?php echo get_option('nanostats_color_daily'); ?>" /></td>
-						<td>The default color for daily words written bars. (default: #000000)</td>
-					</tr>
-					<tr valign="top">
-						<th scope="row">Title</th>
-						<td><input type="text" name="nanostats_title" value="<?php echo get_option('nanostats_title'); ?>" /></td>
-						<td>The title for your graph. Use %u to display the username. Keep empty for no username</td>
-					</tr>
-				</table>
-				<?php submit_button(); ?>
-				<h2>Documentation</h2>
-				You can either have a look at <a href="http://plugins.camilstaps.nl/nanowrimo-stats">this online documentation</a> with examples and other nice stuff, or lookup something in the summary below:
-				<br/>
-				<h3>NaNoWriMo Stats</h3>
-				Adding <pre>[nanostats]</pre> to a post, page or sidebar widget will include your NaNoWriMo stats! 
-				<br/>You can also add variables to this shortcode, like this: <pre>[nanostats width=500]</pre> 
-				<br/>You can use the following variables:
-				<br/>
-				<ul>
-					<li><pre>username</pre>: the username to show the statistics for</li>
-					<li><pre>width</pre>: the width of the graph in pixels</li>
-					<li><pre>height</pre>: the height of the graph in pixels</li>
-					<li><pre>wccolor</pre>: the color for the wordcount bars (don't forget the '#' !)</li>
-					<li><pre>goalcolor</pre>: the color for the goal line (don't forget the '#' !)</li>
-					<li><pre>dailycolor</pre>: the color for the daily words bars (don't forget the '#' !)</li>
-					<li><pre>showtotals</pre>: set to false if you don't want to see the totals (default: true)</li>
-					<li><pre>showgoal</pre>: set to false if you don't want to see the goal line (default: true)</li>
-					<li><pre>showdaily</pre>: set to false if you don't want to see the daily words written bars (default: true)</li>
-					<li><pre>showtitle</pre>: set to false if you don't want a title</li>
-				</ul>
-				All variables you don't define, will get the default value you inputted above.
-				<br/>
-				<h3>NaNoWidgets</h3>
-				You can also easily add NaNoWriMo widgets to your site, using the following shortcodes:
-				<br/>
-				<ul>
-					<li><pre>[nanowidget_participant]</pre>
-						<br/>Additional variables:
-						<ul>
-							<li><pre>show_username</pre>: set to true if you want the widget to display your username</li>
-						</ul>
-						</li>
-					<li><pre>[nanowidget_calendar]</pre></li>
-					<li><pre>[nanowidget_progress]</pre></li>
-					<li><pre>[nanowidget_wordwar]</pre> (you should give the usernames in variables like <pre>username1=example username2=example</pre>)</li>
-					<li><pre>[nanowidget_regionstatus]</pre></li>
-					<li><pre>[nanowidget_regionwar]</pre> (you should give the regions in variables like <pre>region1=example region2=example</pre>)</li>
-				</ul>
-				All widgets have the following variables:
-				<br/>
-				<ul>
-					<li><pre>username</pre>: override the default username setting (only for participant, calendar, progress and wordwar)
-						<br/><pre>region</pre>: override the default region setting (only for regionstatus and regionwar)</li>
-					<li><pre>showpercent</pre>: set to true if you want to see a percentage of your goal</li>
-					<li><pre>showdays</pre>: set to true if you want to see which day of the month it is</li>
-					<li><pre>showwords</pre>: set to true if you want the widget to display your wordcount</li>
-					<li><pre>goal</pre>: give it a number to override the standard 50.000 words goal</li>
-				</ul>
-				<br/>
-				<span style="font-color:#f00;">One of the variables <pre>showpercent</pre>, <pre>showdays</pre>, <pre>showwords</pre> should be set to true in every widget in order for the widget to work.
-				<br/>
-				<h3>Plain-text NaNoWidgets</h3>
-				In case you don't want images and stuff, but just some plain-text stats, use one of the following shortcodes:
-				<br/>
-				<ul>
-					<li><pre>[nanostats_wordcount]</pre>: will be replaced by your wordcount.
-						<br/>Variables:
-						<br/>
-						<ul>
-							<li><pre>username</pre>: override the default username setting</li>
-						</ul>
-						</li>
-				</ul>
-			</form>
-		</div>
-		<?php
+		include(dirname(__FILE__).'/admin.php');
 	}
 }
 
